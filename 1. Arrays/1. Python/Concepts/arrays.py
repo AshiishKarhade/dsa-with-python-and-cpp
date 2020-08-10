6c4cbfc71907bcffb41b481c55cf974ed4c8b7bc
Ashish <akarhade5@gmail.com>
2020-08-10 12:55:45 +0530

    arrays basics done

--- 1. Arrays/1. Python/Concepts/arrays.py
+++ 1. Arrays/1. Python/Concepts/arrays.py
@@ -15,3 +15,12 @@ print(things[1])
 # length/size of the array
 length = len(things) 
 print(length) # length variable holds the size of the array which is in this case, 3
+
+# looping through the array/lists
+for a in things: # iterating over each element of the array
+    print(a) 
+
+for i in range(len(things)): # iterating the variable i upto the length of array
+    print(things[i])        # and then accessing the array element present at index i
+
+
