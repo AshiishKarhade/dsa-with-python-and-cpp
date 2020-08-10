a614c9c6e7fda196d852888b9c3a0228462fb77c
Ashish <akarhade5@gmail.com>
2020-08-10 12:53:27 +0530

    arrays init

---
+++ 1. Arrays/1. Python/Concepts/Concept.md
@@ -0,0 +1,3 @@
+# Arrays
+
+* Python do not have built-in support for the Arrays, but instead Python Lists can be used as arrays in Python
---
+++ 1. Arrays/1. Python/Concepts/arrays.py
@@ -0,0 +1,17 @@
+# Python lists can be used as Arrays in Python
+
+# Creating an array
+things = ['pen', 'car', 'books']
+print(things)
+
+
+# accessing the array element
+print(things[0]) # prints the first element of the array things
+
+# modifying the value of the array element
+things[1] = 'pencil' # second element of things, gets changed to pencil
+print(things[1])
+
+# length/size of the array
+length = len(things) 
+print(length) # length variable holds the size of the array which is in this case, 3
