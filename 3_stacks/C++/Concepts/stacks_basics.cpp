37a1505b21a889fcf11176adb640335c3a10d78d
Ashish <akarhade5@gmail.com>
2020-08-12 12:30:58 +0530

    stacks cpp basics

--- 3_stacks/C++/Concepts/stacks_basics.cpp
+++ 3_stacks/C++/Concepts/stacks_basics.cpp
@@ -39,16 +39,56 @@ class Stacks{
         }
     }
     // SIZE
-    int size(){
+    int f_size(){
         return size;
     }
+    // TOP
+    int peek(){
+        return head->value;
+    }
 
-    void
+    int pop(){
+        int last = 0;
+        if (head==NULL){
+            return 0;
+        }
+        else{
+            last = peek();
+            head = head->next;
+            size--;
+        }
+        return last;
+    }
+
+    bool is_empty(){
+        if(size <= 0){
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
+    void print(){
+        Node* temp = head;
+        while(temp != NULL){
+            cout << temp->value << " ";
+            temp = temp->next;
+        }
+        cout << "\n";
+    }
 };
 
 int main(){
 
-
-
+    Stacks st;
+    st.push(5);
+    st.push(10);
+    st.push(15);
+    st.print();
+    cout << st.f_size() << "\n";
+    st.pop();
+    st.print();
+    cout << st.f_size() << "\n";
     return 0;
 }
