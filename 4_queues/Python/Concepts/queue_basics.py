39b56704adf5dc59989197843d5275d2de0b7022
Ashish <akarhade5@gmail.com>
2020-08-13 20:20:42 +0530

    python queue basics

--- 4_queues/Python/Concepts/queue_basics.py
+++ 4_queues/Python/Concepts/queue_basics.py
@@ -13,7 +13,7 @@ class Queue:
     # i.e pushes from the tail or back of LL
     def push(self, value):
         temp = Node(value)
-        if self.front = None:
+        if self.front == None:
             self.front = temp
             self.back = temp
             self.size = 1
@@ -23,19 +23,42 @@ class Queue:
             self.size += 1
 
     def pop(self):
-        if self.front = None:
+        if self.front == None:
             return
         else:
             last = self.front.value
             self.front = self.front.next
+            self.size -= 1
             return last
 
-if __name == "__main__":
+    def get_size(self):
+        return self.size
+    
+    def is_empty(self):
+        if self.size <= 0:
+            return True
+        else:
+            return False
+    
+    def print_queue(self):
+        temp = self.front
+        while(temp != None):
+            print(temp.value, end=" ")
+            temp = temp.next
+        print()
+
+if __name__ == "__main__":
     q = Queue()
     q.push(5)
     q.push(10)
     q.push(15)
     q.push(20)
+
+    q.print_queue()
+    print(q.get_size())
+    q.pop()
+    q.print_queue()
+    print(q.get_size())
             
 
 
