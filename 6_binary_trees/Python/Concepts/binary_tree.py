c6dfc598945c8fab21be07d20c1988ab8e7dc358
Ashish <akarhade5@gmail.com>
2020-08-14 18:13:24 +0530

    binary tree py

---
+++ 6_binary_trees/Python/Concepts/binary_tree.py
@@ -0,0 +1,40 @@
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
+class BST:
+    def __init__(self, root_val):
+        self.root = Node(root_val)
+    
+    def insert(self, value):
+        newnode = Node(value)
+        if self.root==None:
+            self.root = newnode
+            return
+        
+        current = self.root
+        while True:
+            if value < current.value:
+                if current.left == None:
+                    current.left = newnode
+                    break
+                current = current.left
+            else:
+                if current.right == None:
+                    current.right = newnode
+                    break
+                current = current.right
+
+
+if __name__ == "__main__":
+    bst = BST(7)
+    bst.insert(10)
+    bst.insert(3)
+    bst.insert(5)
+
+    print(bst.root.value)
+    print(bst.root.left.value)
+    print(bst.root.right.value)
+    print(bst.root.left.right.value)
