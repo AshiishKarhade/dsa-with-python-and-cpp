8ae41f139ebed7f457bdd1cf84bf1d9ee7329213
Ashish <akarhade5@gmail.com>
2020-08-15 11:47:10 +0530

    bst traverse complete

--- 6_binary_trees/Python/Concepts/bst_traversal.py
+++ 6_binary_trees/Python/Concepts/bst_traversal.py
@@ -33,11 +33,29 @@ class BST:
         self.pre_order(root.left)
         self.pre_order(root.right)
 
+    def in_order(self, root):
+        if root == None:
+            return
+        self.in_order(root.left)
+        print(root.value, end=" ")
+        self.in_order(root.right)
+
+    def post_order(self, root):
+        if root == None:
+            return
+        self.post_order(root.left)
+        self.post_order(root.right)
+        print(root.value, end=" ")
+
 if __name__ == "__main__":
     bst = BST()
     bst.insert(7)
     bst.insert(10)
     bst.insert(3)
     bst.insert(5)
-
+    print("\nPre-order: ")
     bst.pre_order(bst.root)
+    print("\nIn-order: ")
+    bst.in_order(bst.root)
+    print("\nPost-order: ")
+    bst.post_order(bst.root)
