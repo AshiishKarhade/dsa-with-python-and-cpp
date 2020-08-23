428a11c40c5688d5b7774e56440988075f39bdcb
Ashish <akarhade5@gmail.com>
2020-08-23 15:01:48 +0530

    basic concepts

--- 8_heaps/C++/Concepts/heaps.cpp
+++ 8_heaps/C++/Concepts/heaps.cpp
@@ -5,6 +5,7 @@ using namespace std;
 // We use arrays instead of Linked Lists to implement heaps
 // left_child = parent_idx * 2 + 1
 // right_child = parent_idx * 2 + 2
+// parent = (index-1)/2
 
 int main(){
 
