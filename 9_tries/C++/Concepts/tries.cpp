f01923c1ed4a2af0ebe789e3198ea983ed057b78
Ashish <akarhade5@gmail.com>
2020-08-24 18:29:39 +0530

    tries cpp

--- 9_tries/C++/Concepts/tries.cpp
+++ 9_tries/C++/Concepts/tries.cpp
@@ -1,6 +1,30 @@
 #include <iostream>
 using namespace std;
 
+class Node{
+    public:
+    char value;
+    Node* childs[26];
+    bool isEndofWord;
+    Node(char value){
+        this->value = value;
+        this->isEndofWord = true;
+    }
+};
+
+class Trie{
+    public:
+    Node* root;
+    
+    Trie(){
+        root = new Node(' ');
+    }
+
+    void insert(char ch){
+        Node n = Node(ch);
+
+    }
+};
 
 int main(){
 
