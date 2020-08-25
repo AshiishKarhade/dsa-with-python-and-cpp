54b1b48302feaa844d5fbe52c580721170d1584f
Ashish <akarhade5@gmail.com>
2020-08-25 17:15:11 +0530

    bubble sort cpp done

--- 11_sorting_algorithms/C++/bubble_sort.cpp
+++ 11_sorting_algorithms/C++/bubble_sort.cpp
@@ -1,9 +1,28 @@
 #include <iostream>
 using namespace std;
 
+void bubble_sort(int arr[], int n){
+    for(int i=0; i<n; i++){
+        for(int j=1; j<n; j++){
+            if(arr[j-1] > arr[j]){
+                swap(arr[j-1], arr[j]);
+            }
+        }
+    }
+}
+
+void print(int arr[], int n){
+    for(int i=0; i<n; i++){
+        cout << arr[i] << " ";
+    }
+    cout << "\n";
+}
+
 int main(){
-
-    
-
+    int arr[] = {4, 1, 2, 7, 5, 9, 3};
+    print(arr, 7);
+    bubble_sort(arr, 7);
+    cout << "After Bubble sort: " << "\n";
+    print(arr, 7);
     return 0;
 }
