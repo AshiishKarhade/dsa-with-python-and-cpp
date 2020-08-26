63ee12de697d75a4dbb15c797d49b1b9f49bd10c
Ashish <akarhade5@gmail.com>
2020-08-26 12:05:16 +0530

    selection sort cpp

--- 11_sorting_algorithms/C++/selection_sort.cpp
+++ 11_sorting_algorithms/C++/selection_sort.cpp
@@ -3,13 +3,13 @@ using namespace std;
 
 void selection_sort(int arr[], int n){
     for(int i=0; i<n; i++){
-        int min = arr[i+1];
+        int min_idx = i;
         for(int j=i+1; j<n; j++){
-            if(arr[j] < min){
-                min = arr[j];
-            }
+            if(arr[j] < arr[min_idx]){
+                min_idx = j;
+            }   
         }
-        swap(arr[i], min);
+        swap(arr[i], arr[min_idx]);
     }
 }
 
