b69d1a2f1f7b000dc27c55854d7e212a30e431a8
Ashish <akarhade5@gmail.com>
2020-08-26 13:28:12 +0530

    merge sort done

--- 11_sorting_algorithms/C++/merge_sort.cpp
+++ 11_sorting_algorithms/C++/merge_sort.cpp
@@ -1,17 +1,69 @@
 #include <iostream>
 using namespace std;
 
-void merge_sort(int arr[], int i, int j){
+void merge_sort(int arr[], int n){
+    // BASE CONDITION
+    if (n < 2){
+        return;
+    }
     // divide
-    int mid = (i+j)/2;
+    int mid = n/2;
+    int left[mid];
+    int right[n-mid];
 
+    for(int i=0; i<mid; i++){
+        left[i] = arr[i];
+    }
+    for(int i=mid; i<n; i++){
+        right[i-mid] = arr[i];
+    }
     // conquer
-    auto left = merge_sort(arr, i, mid);
-    auto right = merge_sort(arr, mid+1, j);
+    merge_sort(left, mid);
+    merge_sort(right, n-mid);
 
     // merge
+    int i=0, j=0, k=0;
+    while(i < sizeof(left)/sizeof(left[0]) && j<sizeof(right)/sizeof(right[0])){
+        if(left[i] <= right[j]){
+            arr[k] = left[i];
+            k++;
+            i++;
+        }
+        else{
+            arr[k] = right[j];
+            k++;
+            j++;
+        }
+    }
+    while(i<sizeof(left)/sizeof(left[0])){
+        arr[k++] = left[i++];
+    }
+    while(j<sizeof(right)/sizeof(right[0])){
+        arr[k++] = right[j++];
+    }
+}
 
-    return mid;
+// IMPORTANT MERGING STEP
+void merge_arr(int left[], int right[], int result[]){
+    int i=0, j=0, k=0;
+    while(i < sizeof(left)/sizeof(left[0]) && j<sizeof(right)/sizeof(right[0])){
+        if(left[i] <= right[j]){
+            result[k] = left[i];
+            k++;
+            i++;
+        }
+        else{
+            result[k] = right[j];
+            k++;
+            j++;
+        }
+    }
+    while(i<sizeof(left)/sizeof(left[0])){
+        result[k++] = left[i++];
+    }
+    while(j<sizeof(right)/sizeof(right[0])){
+        result[k++] = right[j++];
+    }
 }
 
 void print(int arr[], int n){
@@ -24,7 +76,7 @@ void print(int arr[], int n){
 int main(){
     int arr[] = {4, 1, 2, 7, 5, 9, 3};
     print(arr, 7);
-    merge_sort(arr, 0, 6);
+    merge_sort(arr, 7);
     cout << "After Insertion sort: " << "\n";
     print(arr, 7);
     return 0;
