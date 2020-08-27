f5c1252de08d2045adc41ab40ddf77526ab7b3ef
Ashish <akarhade5@gmail.com>
2020-08-27 14:02:35 +0530

    ternary cpp skele

--- 12_searching_algorithms/C++/ternary_search.cpp
+++ 12_searching_algorithms/C++/ternary_search.cpp
@@ -1,9 +1,23 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 
+int ternary_search(){
 
+}
+
+void print(int index){
+    if(index<0){
+        cout << "Key is not present" << "\n";
+    }
+    else{
+        cout << "Key is present at: " << index << "\n";
+    }
+}
 
 int main(){
-
+    int arr[] = {12, 23, 35, 46, 57, 69, 82, 92};
+    int idx = ternary_search(arr, 35);
+    print(idx);
     return 0;
 }
