97d6bb3b461256166f6bd740124263e333feb117
Ashish <akarhade5@gmail.com>
2020-08-27 18:35:08 +0530

    count vowels cpp

---
+++ 13_string_manipulation/C++/1_count_vowels.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    string s1;
+    char vowels[] = {'a', 'e', 'i', 'o', 'u'};
+    s1 = "aieou";
+
+    int no_of_vowels = 0;
+    for(int i=0; i<5; i++){
+        for(int j=0; j<s1.length(); j++){
+            if(vowels[i] == s1[j]){
+                no_of_vowels += 1;
+            }
+        }
+    }
+    cout << no_of_vowels << "\n";
+
+    return 0;
+}
