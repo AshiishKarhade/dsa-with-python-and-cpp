fee0c6cdf37eb48a39abfa723640cb1a5493620f
Ashish <akarhade5@gmail.com>
2020-08-27 14:36:34 +0530

    jump search cpp

--- 12_searching_algorithms/C++/jump_search.cpp
+++ 12_searching_algorithms/C++/jump_search.cpp
@@ -1,9 +1,28 @@
 #include <iostream>
-#include <vector>
+#include <cmath>
 using namespace std;
 
-int jump_search(int arr[], int key,int n, int left, int right){
-    
+int jump_search(int arr[], int key, int n){
+    int block_size = sqrt(n);
+    int start = 0;
+    int next = block_size;
+
+    while(arr[next-1] < key){
+        start = next;
+        if(start >= n){
+            break;
+        }
+        next = next + block_size;
+        if(next > n){
+            next = n;
+        }
+    }
+    for(int i=0; i< next; i++){
+        if(arr[i] == key){
+            return i;
+        }
+    }
+    return -1;
 }
 
 void print(int index){
@@ -17,7 +36,7 @@ void print(int index){
 
 int main(){
     int arr[] = {12, 23, 35, 46, 57, 69, 82, 92};
-    int idx = jump_search(arr, 35, 8, 0, 7);
+    int idx = jump_search(arr, 35, 8);
     print(idx);
     return 0;
 }
